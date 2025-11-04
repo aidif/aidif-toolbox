348a84087d93698cd34cde2996fa407d427a2ac8
Jan Wrede <wrede.jan@gmail.com>
2025-11-04 14:19:45 +0100

    Pick random patients (exclude lop for now), resample basal

--- src/AIDIF/processBabelbetes.m
+++ src/AIDIF/processBabelbetes.m
@@ -20,40 +20,67 @@ import AIDIF.constructHiveQueryTable
 
 %% create the import query table for babelbetes hive schema
 %assign root folder for babelbetes data partition in rootFolder variable
-rootFolder = "your/babelbetes/output/path/here";
+rootFolder = "/Users/jan/git/aidif/out";
 queryTable = constructHiveQueryTable(rootFolder);
 
+%TODO: Eclude Loop for now
+queryTable = queryTable(queryTable.study_name ~= "Loop",:);
+
 %% ingest babelbetes data, by study and subject, for all data types.
-% create subset for example processing
-subset = queryTable(ismember(queryTable.study_name,"DCLP3") & ...
-                    ismember(queryTable.patient_id,string(1:10)),:);
-[~,uniquePatient,occurrences] = unique(subset(:,["study_name" "patient_id"]),...
-                                "rows","stable");
 
-for iPatient = 1:numel(uniquePatient)
 
-    relevantPaths = subset(occurrences == uniquePatient(iPatient),:);
-    %TODO Any file verification/alignment needed
-    for iFile = 1:height(relevantPaths)
+%select a random of 6 patients
+nPatients = 25;
+uniquePatients = unique(queryTable(:,["study_name","patient_id"]));
+randomIndexes = randi([1,height(uniquePatients)],nPatients,1);
+randomPatients = uniquePatients(randomIndexes,:);
 
-        currentDataType = relevantPaths.data_type(iFile);
-        rawData = parquetread(relevantPaths.path(iFile),"OutputType","timetable");
 
-        switch currentDataType
-            case "cgm"
-                disp("case 1: " + currentDataType + ...
-                    " data resampled for patient " + string(iPatient))
-            case "basal"
-                disp("case 2: " + currentDataType + ...
-                    " data resampled for patient " + string(iPatient))
-            case "bolus"
-                disp("case 3: " + currentDataType + ...
-                    " data resampled for patient " + string(iPatient))
-            otherwise
-                disp(currentDataType + " file not processed.")
-        end
+randomPatients
+errorLog = {}
+%%
+for iRandomPatient = 1:height(randomPatients)
+    rowMask = ismember(queryTable(:, {'study_name','patient_id'}), randomPatients(iRandomPatient,:));
+    rows = queryTable(rowMask,:);
+    
+    % TODO Any file verification/alignment needed
+    if height(rows)~=3
+        warning("Patient %s from study %s does not have the expected number of data entries.", ...
+                string(randomPatients.patient_id(iRandomPatient)), ...
+                string(randomPatients.study_name(iRandomPatient)));
+        continue
     end
+    if ~all(ismember(rows.data_type,["basal","bolus","cgm"]))
+        warning("Patient %s from study %s has missing data.", string(randomPatients.patient_id(iRandomPatient)), string(randomPatients.study_name(iRandomPatient)))
+        continue
+    end
+    
+    
+    % Resampling
+    try
+        basalPath = rows(rows.data_type=='basal',"path").path;
+        rawBasal = parquetread(basalPath, "OutputType", "timetable");
+        basalResampled = AIDIF.interpolateBasal(rawBasal);
+    
+    catch exception
+        
+        fprintf('Error processing patient %s from study %s: %s\n', ...
+                string(randomPatients.patient_id(iRandomPatient)), ...
+                string(randomPatients.study_name(iRandomPatient)), ...
+                exception.message);
 
+        
+        % Append error details to a structure array
+        errorLog(iRandomPatient).study_name = string(randomPatients.study_name(iRandomPatient));
+        errorLog(iRandomPatient).patient_id = string(randomPatients.patient_id(iRandomPatient));
+        errorLog(iRandomPatient).data_type = 'basal';
+        errorLog(iRandomPatient).error_message = exception.message;
+        continue
+    end
+    
+    basalResampled
+    
+    
     % TODO combine cgm, basal, and bolus functions
 
 end
