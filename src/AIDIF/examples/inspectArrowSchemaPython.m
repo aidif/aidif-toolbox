42b69cccfae7ac650a916b8296906f1b932d5f0c
Jan Wrede <wrede.jan@gmail.com>
2025-11-25 15:18:49 +0100

    Adjust to support Windows machines

--- src/AIDIF/examples/inspectArrowSchemaPython.m
+++ src/AIDIF/examples/inspectArrowSchemaPython.m
@@ -7,7 +7,13 @@
 %[text] pip install -r src/AIDIF/+AIDIF/python/requirements.txt
 %[text] ### Connect Matlab and python
 %[text] Point matlab to the python of the virtual environment we just created
-pyenv('Version',fullfile(pwd,'venv','bin','python')) %[output:6307bf38]
+if ispc
+    pythonPath = fullfile(pwd,'.venv','Scripts','python.exe');
+else
+    pythonPath = fullfile(pwd,'.venv','bin','python');
+end
+pyenv('Version', pythonPath)
+%%
 %[text] ### Create a pandas dataframe and save to parquet
 code = [
 "from datetime import datetime, timedelta" newline ...
@@ -27,8 +33,8 @@ code = [
 
 pyrun(code);
 %[text] ### Reading it with Matlab
-tt = parquetread('python.parquet') %[output:2b81f659]
-parquetinfo('python.parquet') %[output:9144eb4c]
+tt = parquetread('python.parquet') %[output:5f2eb104]
+parquetinfo('python.parquet') %[output:780dc2da]
 %[text] As we dan see, Matlab does not convert delivery duration to the correct unit. That's because it treats it as int64, not as a duration.
 %[text] ### Read the full parquet arrow information (using python)
 file_path = "python.parquet";
@@ -38,13 +44,13 @@ code = [
 "schema_arrow = pq.ParquetFile('" + file_path + "').schema_arrow" newline 
 ];
 arrowSchema = pyrun(code,'schema_arrow');
-disp(arrowSchema) %[output:049d0e68]
+disp(arrowSchema) %[output:1090f199]
 unit = string(arrowSchema.field('delivery_duration').type.unit);
-fprintf("The delivery_duration unit is %s\n", string(unit)); %[output:7c1c1fe3]
+fprintf("The delivery_duration unit is %s\n", string(unit)); %[output:95d2045e]
 %[text] ### Or by calling our python script
 arrowSchema = pyrunfile('readParquetArrowSchema.py','schema',path = 'python.parquet');
 unit = string(arrowSchema.field('delivery_duration').type.unit);
-fprintf("The delivery_duration unit is %s\n", string(unit)); %[output:5cddb43a]
+fprintf("The delivery_duration unit is %s\n", string(unit)); %[output:642c6b8e]
 %[text] ### Now convert the duration
 switch unit
     case "ns"
@@ -55,13 +61,13 @@ switch unit
         error("Unit not expected");
 end
 delivery_duration = milliseconds(tt.delivery_duration/base);
-display(tt) %[output:55ee05d3]
+display(delivery_duration) %[output:77b2c312]
 %%
 %[text] ### Or by calling our convenience function
 base =  AIDIF.readParquetDurationBase('python.parquet','delivery_duration');
-fprintf("Base = %d",base) %[output:20c85e67]
+display(base) %[output:913ebe0b]
 delivery_duration = milliseconds(tt.delivery_duration/base);
-disp(delivery_duration) %[output:3691a4fe]
+display(delivery_duration) %[output:40675f60]
 %[text] ### 
 
 %[appendix]{"version":"1.0"}
@@ -69,30 +75,27 @@ disp(delivery_duration) %[output:3691a4fe]
 %[metadata:view]
 %   data: {"layout":"inline"}
 %---
-%[output:6307bf38]
-%   data: {"dataType":"textualVariable","outputData":{"name":"ans","value":"  <a href=\"matlab:helpPopup('matlab.pyclient.PythonEnvironment')\" style=\"font-weight:bold\">PythonEnvironment<\/a> with properties:\n\n          Version: \"3.11\"\n       Executable: \"\/Users\/jan\/git\/aidif\/aidif-toolbox\/venv\/bin\/python\"\n          Library: \"\/Users\/jan\/.pyenv\/versions\/3.11.0\/lib\/libpython3.11.dylib\"\n             Home: \"\/Users\/jan\/git\/aidif\/aidif-toolbox\/venv\"\n           Status: Loaded\n    ExecutionMode: InProcess\n        ProcessID: \"59632\"\n      ProcessName: \"MATLAB\"\n"}}
+%[output:5f2eb104]
+%   data: {"dataType":"tabular","outputData":{"columnNames":["datetime","bolus","delivery_duration"],"columns":3,"dataTypes":["datetime","double","int64"],"header":"3×3 table","name":"tt","rows":3,"type":"table","value":[["25-Nov-2025 16:15:32","1.2000","1000000000"],["25-Nov-2025 17:15:32","2","120000000000"],["25-Nov-2025 18:15:32","3","10800000000000"]]}}
 %---
-%[output:2b81f659]
-%   data: {"dataType":"tabular","outputData":{"columnNames":["datetime","bolus","delivery_duration"],"columns":3,"dataTypes":["datetime","double","int64"],"header":"3×3 table","name":"tt","rows":3,"type":"table","value":[["24-Nov-2025 17:18:34","1.2000","1000000000"],["24-Nov-2025 18:18:34","2","120000000000"],["24-Nov-2025 19:18:34","3","10800000000000"]]}}
-%---
-%[output:9144eb4c]
+%[output:780dc2da]
 %   data: {"dataType":"textualVariable","outputData":{"name":"ans","value":"  <a href=\"matlab:helpPopup('matlab.io.parquet.ParquetInfo')\" style=\"font-weight:bold\">ParquetInfo<\/a> with properties:\n\n               Filename: \"\/Users\/jan\/git\/aidif\/aidif-toolbox\/python.parquet\"\n               FileSize: 2870\n           NumRowGroups: 1\n        RowGroupHeights: 3\n          VariableNames: [\"datetime\"    \"bolus\"    \"delivery_duration\"]\n          VariableTypes: [\"datetime\"    \"double\"    \"int64\"]\n    VariableCompression: [\"snappy\"    \"snappy\"    \"snappy\"]\n       VariableEncoding: [\"dictionary\"    \"dictionary\"    \"dictionary\"]\n                Version: \"2.0\"\n"}}
 %---
-%[output:049d0e68]
+%[output:1090f199]
 %   data: {"dataType":"text","outputData":{"text":"  Python <a href=\"matlab:helpPopup('py.pyarrow.lib.Schema')\" style=\"font-weight:bold\">Schema<\/a> with properties:\n\n           metadata: [1×1 py.dict]\n              names: [1×3 py.list]\n    pandas_metadata: [1×1 py.dict]\n              types: [1×3 py.list]\n\n    datetime: timestamp[ns]\n    bolus: double\n    delivery_duration: duration[ns]\n    -- schema metadata --\n    pandas: '{\"index_columns\": [{\"kind\": \"range\", \"name\": null, \"start\": 0, \"' + 637\n\n","truncated":false}}
 %---
-%[output:7c1c1fe3]
+%[output:95d2045e]
 %   data: {"dataType":"text","outputData":{"text":"The delivery_duration unit is ns\n","truncated":false}}
 %---
-%[output:5cddb43a]
+%[output:642c6b8e]
 %   data: {"dataType":"text","outputData":{"text":"The delivery_duration unit is ns\n","truncated":false}}
 %---
-%[output:55ee05d3]
-%   data: {"dataType":"tabular","outputData":{"columnNames":["datetime","bolus","delivery_duration"],"columns":3,"dataTypes":["datetime","double","int64"],"header":"3×3 table","name":"tt","rows":3,"type":"table","value":[["24-Nov-2025 17:18:34","1.2000","1000000000"],["24-Nov-2025 18:18:34","2","120000000000"],["24-Nov-2025 19:18:34","3","10800000000000"]]}}
+%[output:77b2c312]
+%   data: {"dataType":"matrix","outputData":{"columns":1,"header":"3×1 duration array","name":"delivery_duration","rows":3,"type":"duration","value":[["1 sec"],["120 sec"],["10800 sec"]]}}
 %---
-%[output:20c85e67]
-%   data: {"dataType":"text","outputData":{"text":"Base = 1000000","truncated":false}}
+%[output:913ebe0b]
+%   data: {"dataType":"textualVariable","outputData":{"name":"base","value":"1000000"}}
 %---
-%[output:3691a4fe]
-%   data: {"dataType":"text","outputData":{"text":"       1 sec\n     120 sec\n   10800 sec\n\n","truncated":false}}
+%[output:40675f60]
+%   data: {"dataType":"matrix","outputData":{"columns":1,"header":"3×1 duration array","name":"delivery_duration","rows":3,"type":"duration","value":[["1 sec"],["120 sec"],["10800 sec"]]}}
 %---
