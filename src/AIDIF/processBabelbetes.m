6e9e1ca0ce7e731de07cc0df5a0f011c45c10a55
Jamie Bate <jamie.h.bate@gmail.com>
2025-12-04 11:36:13 +1300

    suggested test updates for merge of glucose and insulin (#31)
    * suggested test updates for merge of glucose and insulin
    
    * Squashed commit of the following:
    
    - integrate data helper with CGM also
    - basic test that processBabelbetes works
    - updates to test processBabelbetes
    
    * update tests to reflect boundary
    
    * remove constant for now

--- src/AIDIF/processBabelbetes.m
+++ src/AIDIF/processBabelbetes.m
@@ -16,15 +16,17 @@
 %   Copyright (c) 2025, AIDIF
 %   All rights reserved
 
-function results = processBabelbetes(rootFolder,exportRoot)
+function results = processBabelbetes(rootFolder, exportRoot, queryTable)
 
 arguments (Input)
     rootFolder char {mustBeTextScalar}
     exportRoot char {mustBeTextScalar} = ""
+    queryTable table {mustBeNonempty} = table()
 end
 
-    queryTable = AIDIF.constructHiveQueryTable(rootFolder);
-    queryTable = queryTable(queryTable.study_name=='Flair'& ismember(queryTable.patient_id,["1","10","89"]),:);
+    if isempty(queryTable)        
+        queryTable = AIDIF.constructHiveQueryTable(rootFolder);
+    end
     
     [patientRows, patients] = findgroups(queryTable(:,["study_name" "patient_id"]));
     results = patients;
@@ -119,6 +121,7 @@ end
 filePath = fullfile(exportRoot,"study_name=" + studyName,"data_type=combined",...
     "patient_id="+patientID);
 mkdir(filePath)
-parquetwrite(filePath+"\babelbetes_combined.parquet",combinedTT)
+
+parquetwrite(fullfile(filePath,"babelbetes_combined.parquet"),combinedTT)
 done = 1;
 end
---
+++ tests/ProcessBabelbetesTest.m
@@ -0,0 +1,22 @@
+classdef ProcessBabelbetesTest < matlab.unittest.TestCase
+
+    methods (Test)
+
+  
+        function testSuccessfulProcessData(testCase)
+            testQT = DataHelper.getHiveQueryTable("default_inputs");
+                
+            processBabelbetes(DataHelper.DefaultDataAssestDir, DataHelper.DefaultDataOutputDir, testQT);
+
+            outputPath = DataHelper.getCombinedOutputFullPath(DataHelper.DefaultDataOutputDir);
+            
+            testCase.assertEqual(exist(outputPath, "file"), 2);
+            combinedTT = parquetread(outputPath, "OutputType", "timetable");
+            testCase.verifyNotEmpty(combinedTT, "Missing output combined data parquet file");
+            testCase.verifyNotEmpty(combinedTT.egv, "Missing EVG data");
+            testCase.verifyNotEmpty(combinedTT.totalInsulin, "Missing totalInsulin data");
+            testCase.verifyNotEmpty(combinedTT.datetime, "Missing datetime data");
+           
+        end
+    end
+end
