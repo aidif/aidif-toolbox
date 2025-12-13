0933ba073d42f775fbe6c1441c22726f54814eee
Michael Wheelock <michael@wheelockworks.com>
2025-12-12 16:46:08 -0800

    [15-total-insulin] Expand ProcessBabelbetesTest cases.

--- tests/ProcessBabelbetesTest.m
+++ tests/ProcessBabelbetesTest.m
@@ -1,18 +1,61 @@
 classdef ProcessBabelbetesTest < matlab.unittest.TestCase
+    properties
+        testPath = fullfile(pwd,"assets");
+        outputPath = fullfile(pwd,"outputs");
+    end
 
+    methods (Access = private)
+
+        function removeOutputDirectory(testCase)
+            if exist(testCase.outputPath,"dir") == 7
+                rmdir(testCase.outputPath,'s')
+            end
+        end
+    end
+    
+    methods (TestMethodSetup)
+
+        function resetOutputDir(testCase)
+            % clear 'outputs' directory before and after any test.
+            testCase.removeOutputDirectory();
+            testCase.addTeardown(@() testCase.removeOutputDirectory());
+        end
+    end
+    
     methods (Test)
 
   
-        function testSuccessfulProcessData(testCase)
-            testPath = string(what("tests\assets\").path);
-            outputPath = string(what("tests\outputs\").path);
+        function successfulProcessForAllData(testCase)
             
-            AIDIF.processBabelbetes(testPath,"exportPath" ,outputPath);
+            AIDIF.processBabelbetes(testCase.testPath,"exportPath" ,testCase.outputPath);
 
-            outputDir = AIDIF.constructHiveQueryTable(outputPath);
+            outputDir = AIDIF.constructHiveQueryTable(testCase.outputPath);
             
-            testCase.verifyTrue(isfile(outputDir.path));
-            combinedTT = parquetread(outputDir.path, "OutputType", "timetable");
+            testCase.verifyEqual(height(outputDir),4);
+            testCase.verifyTrue(isfile(outputDir.path(1)));
+
+            combinedTT = parquetread(outputDir.path(1), "OutputType", "timetable");
+            testCase.verifyNotEmpty(combinedTT, "Missing output combined data parquet file");
+            testCase.verifyNotEmpty(combinedTT.egv, "Missing EVG data");
+            testCase.verifyNotEmpty(combinedTT.totalInsulin, "Missing totalInsulin data");
+            testCase.verifyNotEmpty(combinedTT.datetime, "Missing datetime data");
+        end
+
+        function successfulProcessWithPatientSubset(testCase)
+            patientTable = AIDIF.constructHiveQueryTable(testCase.testPath);
+            [~,patientTable] = findgroups(patientTable(:,["study_name" "patient_id"]));
+            patientTable = patientTable(patientTable.study_name == "StudyB" & patientTable.patient_id == "Patient2",:);
+
+            AIDIF.processBabelbetes(testCase.testPath,"exportPath",testCase.outputPath,"patientTable",patientTable);
+
+            outputDir = AIDIF.constructHiveQueryTable(testCase.outputPath);
+
+            testCase.verifyEqual(height(outputDir),1);
+            testCase.verifyEqual(outputDir.study_name,"StudyB");
+            testCase.verifyEqual(outputDir.patient_id,"Patient2")
+            testCase.verifyTrue(isfile(outputDir.path(1)));
+
+            combinedTT = parquetread(outputDir.path(1), "OutputType", "timetable");
             testCase.verifyNotEmpty(combinedTT, "Missing output combined data parquet file");
             testCase.verifyNotEmpty(combinedTT.egv, "Missing EVG data");
             testCase.verifyNotEmpty(combinedTT.totalInsulin, "Missing totalInsulin data");
